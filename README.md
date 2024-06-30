7ad5a93bb2012059886be90d8dd4577e9763086e
Mariana <marianar@andrew.cmu.edu>
2024-06-30 13:24:16 -0400

    adding homework sql and python scripts as well as txt answers

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cmu-95797-23m6
+# Data Warehousing 95797 CMU
+
+This is the repository for [Data Warehousing 95797](https://api.heinz.cmu.edu/courses_api/course_detail/95-797). It contains  weekly assigments
+
+## Assignment 1:
+● Install and setup <br>
+● Download [data](s3://cmu-95797-23m2) <br>
+● Load data to duckDB<br>
+● Verify row counts w/ duckdb CLI
+
+
---
+++ scripts/dump_raw_counts.py
@@ -0,0 +1,18 @@
+import duckdb
+
+# Connect to DuckDB
+con = duckdb.connect('main.db')
+
+# Query to get all table names
+table_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main';"
+tables = con.execute(table_query).fetchall()
+
+# Iterate through each table and get the row count
+for table in tables:
+    table_name = table[0]
+    count_query = f"SELECT COUNT(*) FROM {table_name};"
+    row_count = con.execute(count_query).fetchone()[0]
+    print(f"{table_name:<25} {row_count:,}")
+
+# Close the connection
+con.close()
