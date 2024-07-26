ea73a58374f28bb1cfdc9cdbc75638633748d58a
Mariana <marianar@andrew.cmu.edu>
2024-07-26 17:26:49 -0400

    changing to the professor answers

--- nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+++ nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
@@ -1,33 +1,26 @@
-WITH trips_with_boroughs AS (
-    SELECT
-        t.type,
-        t.pickup_datetime,
-        t.dropoff_datetime,
-        t.duration_min,
-        t.duration_sec,
-        t.pulocationid,
-        t.dolocationid,
-        p.borough AS pu_borough,
-        d.borough AS do_borough,
-        WEEKDAY(t.pickup_datetime) AS weekday
-    FROM
-        {{ ref('mart__fact_all_taxi_trips') }} t
-    JOIN
-        {{ ref('mart__dim_locations') }} p ON t.pulocationid = p.locationid --began location
-    JOIN
-        {{ ref('mart__dim_locations') }}d ON t.dolocationid = d.locationid -- end location
-)
-SELECT
-    weekday,
-    COUNT(*) AS total_trips,
-    COUNT(CASE WHEN pu_borough <> do_borough THEN 1 END) AS trips_different_boroughs, -- if began borough <> end borough count as 1 else 0
-    ROUND(
-        COUNT(CASE WHEN pu_borough <> do_borough THEN 1 END) * 100.0 / COUNT(*),
-        2
-    ) AS percentage_different_boroughs
-FROM
-    trips_with_boroughs
-GROUP BY
-    weekday
-ORDER BY
-    weekday;
+-- JOIN strategy, excluding NULL locations
+
+with all_trips as
+(select 
+    weekday(pickup_datetime) as weekday, 
+    count(*) trips
+    from {{ ref('mart__fact_all_taxi_trips') }} t
+    where PUlocationID is not null and DOlocationID is not null
+    group by all),
+
+inter_borough as
+(select 
+    weekday(pickup_datetime) as weekday, 
+    count(*) as trips
+from {{ ref('mart__fact_all_taxi_trips') }} t
+join {{ ref('mart__dim_locations') }} pl on t.PUlocationID = pl.LocationID
+join {{ ref('mart__dim_locations') }} dl on t.DOlocationID = dl.LocationID
+where pl.borough != dl.borough
+group by all)
+
+select all_trips.weekday,
+       all_trips.trips as all_trips,
+       inter_borough.trips as inter_borough_trips,
+       inter_borough.trips / all_trips.trips as percent_inter_borough
+from all_trips
+join inter_borough on (all_trips.weekday = inter_borough.weekday);
