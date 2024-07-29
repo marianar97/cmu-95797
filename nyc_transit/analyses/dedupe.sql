7082570a2cf5503064d90668239e25d443feedfd
Mariana <marianar@andrew.cmu.edu>
2024-07-28 22:36:38 -0400

    fixing bug in dedupe.sql

--- nyc_transit/analyses/dedupe.sql
+++ nyc_transit/analyses/dedupe.sql
@@ -1,13 +1,15 @@
--- Use Window functions with QUALIFY and ROW_NUMBER to remove
--- duplicate rows. Prefer rows with a later time stamp
+-- Uses Window functions with QUALIFY and ROW_NUMBER to remove
+-- duplicate rows. Prefer rows with a later time stamp. 
+-- The definition of a unique does not take into account timestamp
 
 SELECT
     insert_timestamp,
     event_id,
     event_type,
     user_id,
-    event_timestamp,
+    event_timestamp
 FROM 
     events
 QUALIFY
-    ROW_NUMBER() OVER (PARTITION BY insert_timestamp ORDER BY insert_timestamp)  = 1;
+    ROW_NUMBER() OVER (PARTITION BY event_id, event_type, user_id ORDER BY insert_timestamp DESC) = 1;
+
